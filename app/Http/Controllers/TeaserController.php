bb1f124768a62adeb6208f108872feb54bb72f44
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-13 15:26:11 +0200

    initial commit

---
+++ app/Http/Controllers/TeaserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTeaserRequest;
+use App\Http\Requests\UpdateTeaserRequest;
+use App\Models\Teaser;
+
+class TeaserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTeaserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Teaser $teaser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Teaser $teaser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTeaserRequest $request, Teaser $teaser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Teaser $teaser)
+    {
+        //
+    }
+}
