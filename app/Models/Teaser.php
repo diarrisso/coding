d1d2ba6d78afaa66926e1698a471853b85e826ee
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-13 16:37:21 +0200

    enhance Teaser model with soft deletes and user relationship

--- app/Models/Teaser.php
+++ app/Models/Teaser.php
@@ -4,9 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Teaser extends Model
 {
     /** @use HasFactory<\Database\Factories\TeaserFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes ;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'slug',
+        'image_path',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
