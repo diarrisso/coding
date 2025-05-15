b62d47b75263170a875af45616b63bab1bde9173
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-15 17:02:52 +0200

    update header.blade.php to change documentation icon from book to document

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -35,7 +35,7 @@
                 <flux:tooltip :content="__('Documentation')" position="bottom">
                     <flux:navbar.item
                         class="h-10 max-lg:hidden [&>div>svg]:size-5"
-                        icon="book-open-text"
+                        icon="document-text"
                         href="https://laravel.com/docs/starter-kits#livewire"
                         target="_blank"
                         label="Documentation"
@@ -111,7 +111,7 @@
                 {{ __('Repository') }}
                 </flux:navlist.item>
 
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
+                <flux:navlist.item icon="document-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
                 {{ __('Documentation') }}
                 </flux:navlist.item>
             </flux:navlist>
