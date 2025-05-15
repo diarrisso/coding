19a12a3c4b1d8878b8229e565881311827f5bc66
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-15 21:55:09 +0200

    add TeaserNotification for sending email notifications on teaser creation

---
+++ app/Notifications/TeaserNotification.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Teaser;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class TeaserNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Teaser $teaser
+     */
+    public function __construct( private readonly Teaser $teaser)
+    {
+    }
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        $recipientName = $notifiable->name ?? 'Administrator';
+        return (new MailMessage)
+            ->subject('Neue Teaser erstellt')
+            ->greeting('Hallo ' . $recipientName . ',')
+            ->line('Eine Teaser wurde erstellt .')
+            ->line('Überschrift: ' . $this->teaser->title)
+            ->action('Greifen Sie auf diese ', route('teasers.show',  $this->teaser->id))
+            ->line('Danke, dass du unsere App benutzt hast!');
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [
+
+        ];
+    }
+
+}
