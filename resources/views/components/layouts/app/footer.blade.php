942bf144364b91385e9269e9cbb73f67b92c5c5e
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-15 16:56:36 +0200

    add footer component to footer.blade.php

---
+++ resources/views/components/layouts/app/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-primary-base border-t border-zinc-200 dark:border-zinc-700 py-4">
+    <div class="container mx-auto px-4 text-center  text-white font-semibold text-sm">
+        &copy; {{ date('Y') }} Meine App. Alle Rechte vorbehalten.
+    </div>
+</footer>
