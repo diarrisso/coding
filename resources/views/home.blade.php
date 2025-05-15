f41cc2193864e9d30b582de6c6b1a966dc27136f
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-15 17:03:09 +0200

    add home.blade.php for displaying news section with Livewire component

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+
+
+    <div class="py-8 bg-gray-50">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 class="text-3xl md:text-4xl font-bold text-gray-900 mb-6">Unsere Neuigkeiten</h1>
+
+            <livewire:home-teasers />
+        </div>
+    </div>
+
