e7a932fec53bfd25f6282eb67b87c24c761ac849
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-15 17:03:00 +0200

    add Livewire component for creating teasers

---
+++ app/Livewire/Teasers/Create.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Teasers;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.teasers.create')
+            ->layout('components.layouts.app');
+    }
+}
