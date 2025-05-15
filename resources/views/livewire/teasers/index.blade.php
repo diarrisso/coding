a07d4fa1c9f7c78f90a962a0e4084277b95d3091
Mamadi Diarrisso <diarrisso49@gmail.com>
2025-05-15 16:58:20 +0200

    add index.blade.php with home teasers component

---
+++ resources/views/livewire/teasers/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="mt-8 flex justify-center xl:justify-start">
+        <h1 class="text-3xl md:text-5xl font-bold dark:text-white">Unsere Neuigkeiten</h1>
+    </div>
+    <div class="mt-8 py-8 bg-gray-50">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <livewire:home-teasers />
+        </div>
+    </div>
+</div>
